8b8d8185b6f1ba506b13f49fc0113568ac5989a4
nansanhao <389746614@qq.com>
2017-07-27 02:03:02 +0800

    third

--- testmessage.js
+++ testmessage.js
@@ -35,17 +35,105 @@ class Student{
         this.code=code;
     }
 }
-function printSheet() {
-    for(let i=0;i<localStorage,length;i++){
-        let key=localStorage.getItem.key(i);
+function changeStu(inputStr) {
+    let temp=inputStr.slice().split(",");
+    $("#changeTable").empty();
+    $("#changeTable").append(`<tr><td>姓名</td><td>语文成绩</td><td>数学成绩</td><td>英语成绩</td><td>编程成绩</td><td colspan="2">修改项</td></tr>`);
+    let find=0;
+    let str="";
+    for(let i=0;i<localStorage.length;i++){
+        let key=localStorage.key(i);
         let value=localStorage.getItem(key);
         value=JSON.parse(value);
-        let row=document.createElement('tr');
-        let nameCell=document.createElement('td');
-        nameCell.innerHTML=value.name;
-        
+        for(let j=0;i<temp.length;i++){
+            if(temp.indexOf(value.id)>=0){
+                str+=`<tr date-stuId="${value.id}"><td><input class="input-1" id="${value.id}name" type="text" value="${value.name}"></input></td><td><input class="input-1" type="text" value="${value.chinese}"></input></td><td><input type="text" class="input-1" value="${value.math}"></input></td><td><input type="text" class="input-1" value="${value.english}"></input></td><td><input type="text" class="input-1" value="${value.code}"></input></td><td><input class="input-1 blue button"type="button" value="确认修改" id="change${value.id}"></td><td><input class="input-1 blue button"type="button" id="${value.id}" onclick="deleteStudent(this)" value="确认删除"></td></tr>`
+                find=1;
+
+            }
+
+
+        }
     }
+    if(find==1){
+        $("#changeTable").append(str);
+        let a=document.createElement('input');
+        a.type='text';
+        a.value="确认修改"
+    }else {
+        alert("您输入的信息不存在")
+    }
+}
+function deleteStudent(input) {
+    alert(input);
+
+
+
+}
+function printSheet(inputStr) {
+    let temp=inputStr.slice().split(",");
+
+    event.preventDefault();
+    $("#stuTable").empty();
+    $("#stuTable").append(`<tr><td>姓名</td><td>语文成绩</td><td>数学成绩</td><td>英语成绩</td><td>编程成绩</td><td>平均分</td><td>总分</td></tr>`);
+    let sumarray=[];
+    let str="";
+    let find=0;
+
+    for(let i=0;i<localStorage.length;i++){
+        let key=localStorage.key(i);
+        let value=localStorage.getItem(key);
+        value=JSON.parse(value);
+        sumarray.push(calSum(value));
+        for(let j=0;i<temp.length;i++){
+            if(temp.indexOf(value.id)>=0){
+                str+=`<tr><td>${value.name}</td><td>${value.chinese}</td><td>${value.math}</td><td>${value.english}</td><td>${value.code}</td><td>${calAvg(value)}</td><td>${calSum(value)}</td></tr>`
+                find=1;
+            }
+
+
+        }
+    }
+    if(find==1){
+        let summid=calSumMid(sumarray);
+        let sumave=calSumAve(sumarray);
+        str+=`<tr><td colspan="2">总分中位数</td><td colspan="5">${summid}</td></tr>`
+        str+=`<tr><td colspan="2">总平均分</td><td colspan="5">${sumave}</td></tr>`;
+        $("#stuTable").append(str);
+    }else {
+        alert("您输入的信息不存在");
+    }
+
+
+
+
 
 }
 //testMessage('4,4,4,4,5:4,4:8,9:4');
 
+function calSum(student) {
+    return parseFloat(student.chinese)+parseFloat(student.math)+parseFloat(student.english)+parseFloat(student.code);
+}
+function calAvg(student) {
+    return (parseFloat(student.chinese)+parseFloat(student.math)+parseFloat(student.english)+parseFloat(student.code))/4;
+}
+function calSumAve(sumArray) {
+    var sum=0;
+    var sumAve=0;
+    for(var i in sumArray){
+        sum+=sumArray[i];
+    }
+    sumAve=sum/sumArray.length;
+    return sumAve;
+}
+function calSumMid(sumArray) {
+    var sumMid=0;
+    var orderedSumArray=sumArray.slice().sort();
+    if(orderedSumArray.length%2==0){
+        sumMid=(orderedSumArray[orderedSumArray.length/2]+orderedSumArray[(orderedSumArray.length/2)-1])/2
+    }
+    else{
+        sumMid=orderedSumArray[(orderedSumArray.length-1)/2];
+    }
+    return sumMid;
+}
